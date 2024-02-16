be37888c4de59235d7488d4c710589c6de48bb68
Otavio Costa <otaviocosta23212@gmail.com>
2024-02-16 18:09:30 -0300

    first commit

---
+++ aoc1/target/debug/aoc1.d
@@ -0,0 +1,1 @@
+/home/ota/Documents/dev/rust/adventofcode/aoc1/target/debug/aoc1: /home/ota/Documents/dev/rust/adventofcode/aoc1/src/main.rs /home/ota/Documents/dev/rust/adventofcode/aoc1/src/puzzle_input.rs
---
+++ aoc1/target/debug/deps/aoc1-87f31ce52f717ee1.d
@@ -0,0 +1,6 @@
+/home/ota/Documents/dev/rust/adventofcode/aoc1/target/debug/deps/aoc1-87f31ce52f717ee1: src/main.rs src/puzzle_input.rs
+
+/home/ota/Documents/dev/rust/adventofcode/aoc1/target/debug/deps/aoc1-87f31ce52f717ee1.d: src/main.rs src/puzzle_input.rs
+
+src/main.rs:
+src/puzzle_input.rs:
